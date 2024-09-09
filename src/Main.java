f67f60652f8259057041bdc4fcd523489108e4fc
Bjornar Aarset <32538579+b3arrr@users.noreply.github.com>
2024-09-09 23:42:48 +0200

    init git

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+
+
+
+
+//have set color coded grades
+//User should increase and decrease the amount he has done each grade
+
+//create class for managing grades
+//class gradeManager
+//make instance/object of gradeManager for each colored grade
+//gradeManager has arrayList as an attribute
+//gradeManager has color grade as an attribute
+
+//user have options 1. increment grade by 1     2. Decrement grade by 1       3. view current session       4. Save & log       5. Quit
