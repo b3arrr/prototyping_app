b5349bbeb285d39e716056dcafb89513cab2e3a2
Bjornar Aarset <32538579+b3arrr@users.noreply.github.com>
2024-09-10 00:19:00 +0200

    grades

---
+++ src/Grade.java
@@ -0,0 +1,16 @@
+public class Grade {
+    private String grade;
+
+    private int score;
+    Grade(String grade, String color, int score) {
+        this.grade = grade;
+        this.score = score;
+
+    }
+    public String getGrade() {
+        return grade;
+    }
+    public int getScore() {
+        return score;
+    }
+}
---
+++ src/GradeManager.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+class GradeManager {
+    public ArrayList<Grade> grade;
+
+    public void makeGrade() {
+
+    }
+
+    public void addGrade(Grade newGrade) { //adds new student references the grade java class
+        String whiteColor = "White";
+        int whiteScore =  0;
+        String greenColor = "Green";
+        int greenScore = 0;
+        String blueColor = "Blue";
+        int blueScore = 0;
+        String yellowColor = "Yellow";
+        int yellowScore = 0;
+        String redColor = "Red";
+        int redScore = 0;
+        String blackColor = "Black";
+        int blackScore = 0;
+
+
+
+    }
+}
+
